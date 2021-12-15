0b852d1ce886391cddc2e1c92d0276e71ace0710
different021 <differentg021@gmail.com>
2021-12-15 22:58:33 +0900

    function performance improvement comp() string -> int calculate

--- src/sort2_fxxxBigNumber/sort2.cpp
+++ src/sort2_fxxxBigNumber/sort2.cpp
@@ -41,16 +41,15 @@
 using namespace std;
 
 bool comp(const int& a, const int& b);
-bool comp2(const int& a, const int& b);
-bool GetI(int& out, int index, int number);
-bool GetNDigitNumber(int& result, int& outMoreDigit, int num, int nDigit);
+int AppendInt(const int& head, const int& tail);
+
 bool GetDigit(int& out, int number);
 
 string solution(vector<int> numbers) {
     string answer = "";
     size_t stringSize = 0;
 
-    sort(numbers.begin(), numbers.end(), comp2);
+    sort(numbers.begin(), numbers.end(), comp);
     
     if (numbers.at(0) == 0) {
         //첫번째 수가 0이면 모두 0이다.
@@ -67,154 +66,65 @@ lb_return:
     return answer;
 }
 
-//파라미터가 1000이하인지 검증하지 않는다. 
-//파라미터 1000 검증이 완료된 상태라고 가정
-/*
-    1. 각각 몇자리 수인지 계산
-    
-    2. 자릿수 비교
-    3. 큰쪽이 이김
-    4. 같은 경우
-     4_1. 다음 자릿수
-     4_2. 다음 자릿수가 없는 경우는 그대로 유지
-    
-
- */
 bool comp(const int& a, const int& b) {
     bool bResult = false;
-    int numberA = a;
-    int numberB = b;
-    int iA = 0;
-    int iB = 0;
-
-    for (int i = 0; i < 4; i++) {
-        GetI(iA, i, numberA);
-        GetI(iB, i, numberB);
-
-        if ((iA < 0) || (iB < 0) ) {
-            //두수의 곱이 음수일 경우
-            //둘 중 하나 outofIndex
-            if (iA == 0)
-            {
-                //bResult = false;
-                break;
-            }
-            else if (iB == 0) {
-                bResult = true;
-                break;
-            }
-
-        }
-
-        if (iA > iB) {
-            bResult = true;
-            break;
-        }
-        else if (iA < iB) {
-            //bResult = false;
-            break;
-        }
-        
-        //두 수가 같을 때만 반복
+    int A = 0;
+    int B = 0;
+    
+    if (a == 0)
+    {
+        goto lb_return;
     }
-    
+
+    if (b == 0) {
+        bResult = true;
+        goto lb_return;
+    }
+
+    if (a == b) {
+        goto lb_return;
+    }
+
+    A = AppendInt(a, b);
+    B = AppendInt(b, a);
+
+    if (A > B)
+        bResult = true;
+
 lb_return:
-    return bResult;
+    return bResult;;
 }
 
-bool comp2(const int& a, const int& b) {
-    string strA = to_string(a);
-    string strB = to_string(b);
-    string firstA, firstB;
-    
-
-    firstA = strA + strB;
-    firstB = strB + strA;
-
-    int A = atoi(firstA.c_str());
-    int B = atoi(firstB.c_str());
-
-    return A > B;
-}
-//가장 큰자리 인덱스 0
 /*
-    [parameter]
-    out     : src의 index번째 숫자 리턴
-    index   : 구하길 원하는 자리 (가장 큰 자릿수 부터 0)
-    number  : 계산할 대상이 될 수
-    
-    [return]
-    bool : 함수 성공 여부
+    제시된 두 정수형 타입을 연결한 정수를 만드는 함수
 
-    ---------------중요------------------
-    [outofIndex일 경우]
-     out = -1
 
-    ex)
-    index   |   0123
-    num     |   5872
-
-        (num)   (index)
-    숫자 5872에서 0번째 수는 5
-    숫자 5872에서 2번째 수는 7
-    숫자 5872에서 7번째 수는 5(가장 앞 자리 수)
-    
 */
-bool GetI(int& out, int index, int number) {
-    bool bResult = false;
-    int num = number;
-    int digit = 0;
-    int result = -1;
-    int div = 1;
+int AppendInt(const int& head, const int& tail)
+{
+    int nResult = -1;
+    int nMul = 1;
+    int nDigitTail = 0;
 
-    if (number < 0) goto lb_return;
-
-    GetDigit(digit, num);
-
-    if (digit - 1 < index) {
-        //가장 앞자리 수 리턴
-        for (int i = 0; i < digit - 1; i++) {
-            div *= 10;
-        }
-        //result = num / div;
-        result = -1;
+    if (head < 0) goto lb_return;
+    if (tail < 0) goto lb_return;
+    if (head == 0 && tail == 0) 
+    {
+        nResult = 0;
+        goto lb_return;
     }
-    else {
-        for (int i = 0; i < digit - index - 1; i++) {
-            div *= 10;
-        }
-        int overOne = num / (div);
-        result = overOne % 10;
-    }
-        
-    bResult = true;
-    out = result;
-lb_return:
-    return bResult;
-}
-
-bool GetNDigitNumber(int& result, int& outMoreDigit, int num, int nDigit) {
-    bool bResult = false;
-    int number = num;
-    int iDigitA = 0;
-    int moreA = 0;
-    int cnt = 1;
-
-    if (number < 0) goto lb_return;
-
-    GetDigit(iDigitA, number);
-    moreA = nDigit - iDigitA;
-
-    for (int i = 0; i < moreA; i++) {
-        cnt *= 10;
+    
+    //오버플로 예방책 추가할 것.
+    GetDigit(nDigitTail, tail);             //자리수 확인
+    for (int i = 0; i < nDigitTail; i++) 
+    {
+        nMul *= 10;
     }
 
-    result = number * cnt;
-    outMoreDigit = moreA;
-    bResult = true;
+    nResult = head * nMul + tail;
 
 lb_return:
-    return bResult;
+    return nResult;
 }
 
 bool GetDigit(int& out, int number) {
@@ -240,8 +150,8 @@ int main()
     //vector<int> arr = { 3, 30 };
     //vector<int> arr = { 0, 0, 0, 1 };
     //vector<int> arr = { 67, 676, 677 };   //67767676
-    //vector<int> arr = { 67, 676 };
-    vector<int> arr = { 0, 0, 0, 0 };
+    vector<int> arr = { 67, 676 };
+    //vector<int> arr = { 0, 0, 0, 0 };
     //vector<int> arr = { 3, 30, 34, 5, 9};
     //vector<int> arr = { 10, 101 };
     //vector<int> arr = { 100, 11, 1, 101 };
