174cb463b1df821064d273a04d07245796d1fc1b
different021 <differentg021@gmail.com>
2022-01-03 22:07:46 +0900

    [DONE]greedy1_suit up

--- src/greedy1_lostSuit/greedy1_lostSuit.cpp
+++ src/greedy1_lostSuit/greedy1_lostSuit.cpp
@@ -33,12 +33,12 @@
 */
 
 /*
+    [solution]
     1. 여벌 체크
     2. 도난 체크 ( 여벌이 있으면 여벌을 본인으로 옮긴다.)
     3. 대여 가능 체크 (본인, 앞번호, 뒷번호)
-
-    1 ~ 2 반복
-    
+        -> 대여시, spare = false
+        -> 대여 대상 인덱스 저장
 */
 
 
@@ -47,32 +47,24 @@
 
 using namespace std;
 
-
-enum eTargetBorrow
-{
-    eBorrowSelf = 0,
-    eBorrowFront,
-    eBorrowBack,
-    eBorrowMAX,
-};
-
 struct stStudent
 {
-    
-    int id;             //출석번호
-    int spareOwner;     //여벌일경우 소유자. 0 본인, -1 본인 스페어, n 여벌 주인 출석 번호
     bool suit;
     bool spare;         //여벌
+    int id;             //출석번호
+    int spareOwner;     //여벌일경우 소유자. 0 본인, -1 본인 스페어, n 여벌 주인 출석 번호
 
     stStudent()
     {
         suit = true;
         spare = false;
+        id = -1;
         spareOwner = 0;
     }
 };
 
 //student 함수들
+void studentInitialize(stStudent& student, int id);
 bool studentSetSpare(stStudent& student, bool hasSpare);
 bool studentStolenSuit(stStudent& student);
 bool studentBorrowSuit(stStudent& borrower, stStudent& lender);
@@ -80,17 +72,18 @@ bool studentBorrowSuit(stStudent& borrower, stStudent& lender);
 
 //solution 함수들
 int solution(int n, vector<int> lost, vector<int> reserve);
-bool solutionInit(stStudent* &pStudents, int numOfStudents);
-bool solutionUpdateSpare(stStudent* pStudents, int numOfStudents, vector<int>& spare);
-bool solutionUpdateLost(stStudent* pStudents, int numOfStudent, vector<int>& lost);
+bool solutionInitialize(vector<stStudent>& students, int numOfStudent);
+bool solutionUpdateSpare(vector<stStudent>& students, vector<int>& spare);
+bool solutionUpdateLost(vector<stStudent>& students, vector<int>& lost);
 bool solutionBorrowSpare(stStudent& me, stStudent* pSmall, stStudent* pLarge); 
-bool solutionBorrowSpareMaximum(stStudent* pStudents, int numOfStudnet);
-int  solutionGetNumberOfStudentHasSuit(stStudent* pStudents, int numOfStudents);
+bool solutionBorrowSpareMaximum(vector<stStudent>& students);
+int  solutionGetNumberOfStudentHasSuit(vector<stStudent>& students);
 
 int solution(int n, vector<int> _lost, vector<int> reserve) {
     int answer = 0;
-    stStudent* pStudents = nullptr;
-    int numOfStudents = n;
+    //stStudent* students = nullptr;
+    vector<stStudent> students;
+    int MAX_STUDENT = n;
     vector<int>& lost = _lost;
     vector<int>& spare = reserve;
 
@@ -98,32 +91,39 @@ int solution(int n, vector<int> _lost, vector<int> reserve) {
     else
     {   
         //필요 데이터 생성
-        solutionInit(pStudents, numOfStudents); //메모리 할당 로직 포함
-        //if(pStudents == nullptr) {/* 메모리 할당 실패 */};
+        solutionInitialize(students, n); //메모리 할당 로직 포함
     }
     
     //여벌 체크
-    solutionUpdateSpare(pStudents, numOfStudents, spare);
+    solutionUpdateSpare(students, spare);
 
     //도난 체크
-    solutionUpdateLost(pStudents, numOfStudents, lost);
+    solutionUpdateLost(students, lost);
 
     //빌리기 체크
-    solutionBorrowSpareMaximum(pStudents, numOfStudents);
+    solutionBorrowSpareMaximum(students);
     
     //수업 가능 인원
-    answer = solutionGetNumberOfStudentHasSuit(pStudents, numOfStudents);
-
-
-
-    //Free Memory
-    delete[] pStudents;
+    answer = solutionGetNumberOfStudentHasSuit(students);
 
     return answer;
 }
 
 
 //학생 클래스 함수 시작
+
+/*
+    @detail 생성후 초기화 코드
+    @param  student destination id keyValue
+    @return void
+*/
+void studentInitialize(stStudent& student, int id)
+{
+    student.id = id;
+    student.suit = true;
+    student.spare = false;
+    student.spareOwner = 0;
+}
 
 /*
     @detail 학생의 스페어 보유 어부 없데이트
@@ -151,7 +151,6 @@ bool studentStolenSuit(stStudent& student)
         student.suit = true;
     }
 
-lb_return:
     return student.suit;    
 }
 
@@ -164,13 +163,15 @@ bool studentBorrowSuit(stStudent& borrower, stStudent& lender)
 {
     bool bResult = false;
     
-    if (borrower.suit = true) goto lb_return;   //빌릴 필요 없다.
-    if (lender.spare = false) goto lb_return;   //빌려줄 물건이 없다.
+    if (borrower.suit == true) goto lb_return;   //빌릴 필요 없다.
+    if (lender.spare == false) goto lb_return;   //빌려줄 물건이 없다.
 
     borrower.suit = true;
     borrower.spareOwner = lender.id;    //주인 표시, 돌려줘야지.
 
+    //lender.spare = false;
     studentSetSpare(lender, false);
+    bResult = true;
 
 lb_return:
     return bResult;
@@ -178,25 +179,20 @@ lb_return:
 
 /*
     @detail 메모리 할당 및 초기화(출석번호 입력)
+    @param student 초기화 시킬 배열, n 생성할 학생 수.
 */
-bool solutionInit(stStudent* &pStudents, int n)
+bool solutionInitialize(vector<stStudent>& students, int n)
 {
     bool bResult = false;
 
     if (n < 1) goto lb_return;
-    if (pStudents != nullptr)
-    {
-        //포이터에 뭔가 있으면 해제.
-        delete[] pStudents;
-        pStudents = nullptr;
-    }
 
-    pStudents = new stStudent[n];
 
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i < n; i++)
     {
-        pStudents[i].id = i + 1;
-        
+        stStudent stu;
+        studentInitialize(stu, i + 1);
+        students.push_back(stu);
     }
 
     bResult = true;
@@ -207,37 +203,36 @@ lb_return:
 
 /*
     @detail 여분의 체육복이 있는 학생 상황 없데이트
-    @param  pStudents 학생 배열 numOfStudents 학색 수 spare 여분의 체육복이 있는 학색 출석번호 리스트
+    @param  students 학생 배열, numOfStudents 학색 수 spare, 여분의 체육복이 있는 학색 출석번호 리스트
     @return 함수 실패 여부, 유효하지 않은 파라미터 입력시 실패
 */
-bool solutionUpdateSpare(stStudent* pStudents, int numOfStudents, vector<int>& spare)
+bool solutionUpdateSpare(vector<stStudent>& students, vector<int>& spare)
 {
     bool bResult = false;
-    int max = numOfStudents - 1;
-    if (pStudents == nullptr) goto lb_return;
-    if (max < 0) goto lb_return;
+    
+    if (students.size() < 0) goto lb_return;
 
     for (auto& it : spare)
     {
         int index = it - 1;
-        studentSetSpare(pStudents[index], true);    //여분 있음.
+        if (index < 0) continue;
+        studentSetSpare(students.at(index), true);    //여분 있음. -> vector변환. 인덱스 검증.
     }
-
 
 lb_return:
     return bResult;
 }
 
 /*
-
+    @detail 체육복을 도난 당한 학색을 suit를 false로 바꿔준다.
+    @param  student 학생들, lost 체육복을 도난 당한 학행들의 출석번호들(int)
 */
-bool solutionUpdateLost(stStudent* pStudents, int numOfStudent, vector<int>& lost)
+bool solutionUpdateLost(vector<stStudent>& students, vector<int>& lost)
 {
     bool bResult = false;
-    int max = numOfStudent - 1;
+    int max = students.size();
 
-    if (pStudents == nullptr) goto lb_return;
-    if (max < 0) goto lb_return;
+    if (students.size() == 0) goto lb_return;
 
     for (auto& it : lost)
     {
@@ -249,10 +244,8 @@ bool solutionUpdateLost(stStudent* pStudents, int numOfStudent, vector<int>& los
             goto lb_return;
         }
 
-        //학생 포인트 널 체크 추가.
-
         //도난
-        studentStolenSuit(pStudents[index]);    //도난 여부 체크, 여분이 있으면 전환.
+        studentStolenSuit(students.at(index));    //도난 여부 체크, 여분이 있으면 전환.
     }
 
     
@@ -265,10 +258,12 @@ lb_return:
     @detail 앞번호 학생 혹은 뒷번호 학생에게 체육복 빌리기. 앞 혹은 뒤 번호가 없으면, NULL (3 파라미터 모두에 데이텨 변환 가능성이 있다. 높다.)
     @param  me 본인, pSmall 앞 번호(NULL 허용), pLarge 뒷 번호(NULL 혀용)
     @return 앞 혹은 뒤 번호에 체육복 빌리기를 성공하면 true, 둘다 없으면 실패
+    @warnning 앞 뒤 번호가 없는 경우(index의 앞과 끝은 앞번호 뒷번호가 없다. 이경우 그냥 nullptr이 들어가면 된다.)
 */
 bool solutionBorrowSpare(stStudent& me, stStudent* pSmall, stStudent* pLarge)
 {
     bool bSuccess = false;
+
     //본인 전환은 도둑 맞은 것을 업데이트 했을 때 실시함.
     //앞번호부터 확인
     if (pSmall != nullptr)
@@ -277,13 +272,16 @@ bool solutionBorrowSpare(stStudent& me, stStudent* pSmall, stStudent* pLarge)
         if (bSuccess == true) goto lb_return;
     }
 
-    //앞번호에 빌리는데 실패.
-    //뒷번호에 빌린다.
-    if (bSuccess == false)
+    //앞번호에 빌리는데 실패 할 경우,
+    //뒷번호한테 빌린다.
+    if (pLarge != nullptr)
     {
-        bSuccess = studentBorrowSuit(me, *pLarge);
+        if (bSuccess == false)
+        {
+            bSuccess = studentBorrowSuit(me, *pLarge);
+        }
     }
-
+    
 lb_return:
     return bSuccess;
 }
@@ -291,40 +289,51 @@ lb_return:
 
 /*
     @detail 수업받을 수 있는 최대 숫자로 학생들 끼리 체육복을 빌리는 행위
-    @param  pStudents 학생들 배열, numOfStudent 학생 수
+    @param  students 학생들 배열, numOfStudent 학생 수
     @return 잘못된 파라미터 입력시 false
 */
-bool solutionBorrowSpareMaximum(stStudent* pStudents, int numOfStudnet)
+bool solutionBorrowSpareMaximum(vector<stStudent>& students)
 {
     bool bResult = false;
-    int num = numOfStudnet;
-    for (int i = 0; i < num; i++)
+    int num = students.size();
+
+    stStudent* pTemp = nullptr;
+    
+
+    for(size_t i = 0; i < students.size(); i++)
     {
-        int small = i - 1;
-        int large = i + 1;
+        size_t small = i - 1;
+        size_t large = i + 1;
         stStudent* pSmall = nullptr;
         stStudent* pLarge = nullptr;
         
         //인덱스가 유효성 검사.
-        if (small > -1)  pSmall = &pStudents[small];        
-        if (large < num) pLarge = &pStudents[large];
-
-        solutionBorrowSpare(pStudents[i], pSmall, pLarge);
+        if (i > 0)  pSmall = &students[small];        
+        if (i < students.size() - 1) pLarge = &students[large];
+        if (i == 1)
+        {
+            int a = 3;
+        }
+        bool isBorrow = solutionBorrowSpare(students.at(i), pSmall, pLarge);
+        int a = 1;
     }
 
     return bResult;
 }
 
-int  solutionGetNumberOfStudentHasSuit(stStudent* pStudents, int numOfStudents)
+/*
+    @detail 체육복을 확보한 최대 학생 수. 
+    @return int 체육복을 확보한(student.suit == true)인 학생 수 리턴
+*/
+int  solutionGetNumberOfStudentHasSuit(vector<stStudent>& students)
 {
     int answer = 0;
-    int max = numOfStudents - 1;
-    if (numOfStudents < 0) goto lb_return;
-    if (pStudents == nullptr) goto lb_return;
+    int max = students.size();
+    if (max < 0) goto lb_return;
 
-    for (int i = 0; i < max; i++)
+    for (auto& it : students)
     {
-        if (pStudents[i].suit == true)
+        if (it.suit == true)
         {
             answer++;
         }
@@ -337,9 +346,15 @@ lb_return:
 int main()
 {
     //return 5 - > 테스트 케이스 실패 다시 검토.
+    /*int n = 5;
+    vector<int> lost = { 2, 4 };
+    vector<int> reserve = { 1, 3, 5 };*/
+
+    //return 4
     int n = 5;
     vector<int> lost = { 2, 4 };
-    vector<int> reserve = { 1, 3, 5 };
+    vector<int> reserve = { 3 };
+
 
     int result = solution(n, lost, reserve);
 
